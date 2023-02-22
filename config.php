42c3b0e44611c1e30eb472e5bd907ac6658dd7e1
Emre Karademir <99919329+emrekarademirx@users.noreply.github.com>
2023-02-23 01:07:45 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Kimlik Doğrulama Anahtarları
+define('CLIENT_ID', 'your-client-id');
+define('CLIENT_SECRET', 'your-client-secret');
+define('REFRESH_TOKEN', 'your-refresh-token');
+
+// Merchant Center Hesap Bilgileri
+define('MERCHANT_ID', 'your-merchant-id');
+define('COUNTRY_CODE', 'your-country-code');
+
+?>
